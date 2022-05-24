579483e616df9596634b7f30c9c712b6083fd9ad
Peter Yoachim <yoachim@uw.edu>
2022-05-24 11:20:42 -0700

    sh

---
+++ technical/ddf_fraction/ddf_frac.sh
@@ -0,0 +1,6 @@
+python ddf_frac.py --sequence_limit 300
+python ddf_frac.py --sequence_limit 286
+python ddf_frac.py --sequence_limit 272
+python ddf_frac.py --sequence_limit 257
+python ddf_frac.py --sequence_limit 243
+python ddf_frac.py --sequence_limit 229
