e3dc4f909c61f62d63396dca4cfcbead450d450f
Peter Yoachim <yoachim@uw.edu>
2022-06-27 12:50:25 -0700

    ddf

---
+++ ddf_season_lengths/ddf.sh
@@ -0,0 +1,6 @@
+python ddf_sl.py --ddf_season_frac 0
+python ddf_sl.py --ddf_season_frac 0.05
+python ddf_sl.py --ddf_season_frac 0.1
+python ddf_sl.py --ddf_season_frac 0.2
+python ddf_sl.py --ddf_season_frac 0.3
+python ddf_sl.py --ddf_season_frac 0.4
