9fa98ef984d34bf260e999b6de5038496c9de703
Peter Yoachim <yoachim@uw.edu>
2022-10-06 15:29:34 -0700

    generate solar system and break it up a bit

---
+++ maf/generate_slurms.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+find ../ -type f \( -iname "*10yrs.db" ! -path "*technical*" \) | xargs -I'{}' ln -s '{}' .
+generate_ss
+cat ss_script.sh >> maf_ss.sh
+generate_ss --pop vatiras_granvik_10k
+cat ss_script.sh >> maf_ss.sh
+split -l 56  maf_ss.sh sub_
+python generate_slurms.py
