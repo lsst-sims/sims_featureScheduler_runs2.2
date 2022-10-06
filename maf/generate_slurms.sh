b8d3b8323f17e221cefa59f12ce900134aa90c7c
Peter Yoachim <yoachim@uw.edu>
2022-10-06 15:38:09 -0700

    shorten up template

--- maf/generate_slurms.sh
+++ maf/generate_slurms.sh
@@ -3,7 +3,7 @@
 
 find ../ -type f \( -iname "*10yrs.db" ! -path "*technical*" \) | xargs -I'{}' ln -s '{}' .
 generate_ss
-cat ss_script.sh >> maf_ss.sh
+cat ss_script.sh > maf_ss.sh
 generate_ss --pop vatiras_granvik_10k
 cat ss_script.sh >> maf_ss.sh
 split -l 56  maf_ss.sh sub_
