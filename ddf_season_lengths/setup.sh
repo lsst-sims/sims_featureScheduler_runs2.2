c67d9cee96f90b575da12772ddc1f0aba31fe543
Peter Yoachim <yoachim@uw.edu>
2022-10-12 14:26:07 -0700

    fucking hell

---
+++ ddf_season_lengths/setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cd /gscratch/scrubbed/yoachim/sims_featureScheduler_runs2.2/ddf_season_lengths 
+source ~/anaconda3/etc/profile.d/conda.sh
+conda activate guro
+export OPENBLAS_NUM_THREADS=1
