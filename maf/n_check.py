87080988bb6e6f7d2ea79e1ba2c28a23a10a7351
Peter Yoachim <yoachim@uw.edu>
2022-10-14 14:13:18 -0700

    limit

--- maf/n_check.py
+++ maf/n_check.py
@@ -5,5 +5,6 @@ if __name__ == '__main__':
     dirs = glob.glob('*yrs_ddf')
     for dirname in dirs:
         files = glob.glob(dirname+'/*')
-        print(dirname, len(files))
+        if len(files) != 573:
+            print(dirname, len(files))
 
